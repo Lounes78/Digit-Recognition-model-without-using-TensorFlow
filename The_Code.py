d0a2f93ec78a03756ee159574e567382f87e18f2
Lounes <98703089+Lounes78@users.noreply.github.com>
2023-10-03 16:27:23 +0200

    Update The_Code.py

--- The_Code.py
+++ The_Code.py
@@ -3,7 +3,7 @@ import pandas as pd
 from matplotlib import pyplot as plt
 
 #Data Preprocessing/Preparation
-data = pd.read_csv('mnist_train.csv')
+data = pd.read_csv('data/mnist_train.csv')
 
 data = np.array(data) #each line represent an image / the first column contains the label / 60000*785
 m, n = data.shape
@@ -24,6 +24,7 @@ X_train = X_train / 255.
 _, m_train = X_train.shape
 
 
+
 # Coding the NN
 
 def init_params():
@@ -32,6 +33,7 @@ def init_params():
 	W2 = np.random.rand(10, 10) - 0.5
 	b2 = np.random.rand(10, 1) - 0.5 
 	return W1, b1, W2, b2
+
 
 def ReLU(Z):
 	return np.maximum(Z, 0)
@@ -47,6 +49,7 @@ def forward_prop(W1, b1, W2, b2, X):
 	Z2 = W2.dot(A1) + b2
 	A2 = softmax(Z2)
 	return Z1, A1, Z2, A2
+
 
 def ReLU_deriv(Z):
 	return Z > 0  # Quite elegant
@@ -64,7 +67,6 @@ def backward_prop(Z1, A1, Z2, A2, W1, W2, X, Y):
 	dZ2 = A2 - one_hot_Y
 	dW2 = 1 / m * dZ2.dot(A1.T)
 	db2 = 1 / m * np.sum(dZ2)
-
 	dZ1 = W2.T.dot(dZ2) * ReLU_deriv(Z1)
 	dW1 = 1 / m * dZ1.dot(X.T)
 	db1 = 1 / m * np.sum(dZ1)
@@ -83,27 +85,59 @@ def get_predictions(A2):
 	return np.argmax(A2, 0)  #Returns the indice of the max probability aka the prediction 
 
 def get_accuracy(predictions, Y):
-	print(predictions, Y)
 	return np.sum(predictions == Y) / Y.size
 
 def gradient_descent(X, Y, alpha, iterations):
 	W1, b1, W2, b2 = init_params()
-
 	for i in range (iterations):
 		Z1, A1, Z2, A2 = forward_prop(W1, b1, W2, b2, X)
 		dW1, db1, dW2, db2 = backward_prop(Z1, A1, Z2, A2, W1, W2, X, Y) 
 		W1, b1, W2, b2 = update_params(W1, b1, W2, b2, dW1, db1, dW2, db2, alpha)
-
 		if i % 10 == 0:
 			print("iteration: ", i)
 			print("Accuracy: ", get_accuracy(get_predictions(A2), Y))
 	return W1, b1, W2, b2
 
-#A chaque iteration on realise une epoch
 
-W1, b1, W2, b2 = gradient_descent(X_train, Y_train, 0.10, 5000)
+# Training
+W1, b1, W2, b2 = gradient_descent(X_train, Y_train, 0.10, 500)
 
 
-# Validation Part
-# Exploit the gradient descent results
 
+
+def make_prediction(X, W1, W2, b1, b2):
+	_, _, _, A2 = forward_prop(W1, b1, W2, b2, X)
+	prediction = get_predictions(A2)
+	return prediction  
+
+
+def test_prediction(index, W1, W2, b1, b2):
+	prediction = make_prediction(X_train[:, index, None], W1, W2, b1, b2) #Gives the prediction
+	true_val = Y_train[index] # The label
+	print("Prediction: ", prediction)
+	print("Label: ", true_val)
+
+	current_image = X_train[:, index, None] #None  is used to ensure that the selected has the right shape and can be properly reshaped and displayed as an image
+	current_image = current_image.reshape((28, 28)) * 255
+	plt.gray()
+	plt.imshow(current_image)
+	plt.show()
+
+
+# Let's try it out
+test_prediction(0, W1, W2, b1, b2)
+test_prediction(1, W1, W2, b1, b2)
+test_prediction(2, W1, W2, b1, b2)
+test_prediction(3, W1, W2, b1, b2)
+test_prediction(4, W1, W2, b1, b2)
+test_prediction(5214, W1, W2, b1, b2)
+
+
+# Accuracy for the dev set
+def test_on_dev_set(X_dev, Y_dev, W1, W2, b1, b2):
+	predictions = make_prediction(X_dev, W1, W2, b1, b2)
+	accuracy = get_accuracy(predictions, Y_dev)
+
+	print("Accuracy: ", accuracy)
+
+test_on_dev_set(X_dev, Y_dev, W1, W2, b1, b2)
